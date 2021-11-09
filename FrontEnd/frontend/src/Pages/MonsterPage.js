dc5725c51ab2c33915e336e80bc9c0bf8cf80708
Tammas Hicks <50436881+ScriptkidHicks@users.noreply.github.com>
2021-11-08 23:05:37 -0800

    added a guide file for navigating the front end

--- FrontEnd/frontend/src/Pages/MonsterPage.js
+++ FrontEnd/frontend/src/Pages/MonsterPage.js
@@ -1,7 +1,17 @@
+import { useContext } from "react";
 import styled from "styled-components";
+import { ColorContext } from "../ContextProviders/ColorContext";
 
 function MonsterPage() {
-  return <div style={{ color: "white" }}>Monster Page</div>;
+  const theme = useContext(ColorContext);
+  return <Body theme={theme}>Monster Page</Body>;
 }
 
 export default MonsterPage;
+
+const Body = styled.div`
+  background-color: ${(props) => props.theme.primaryBackground};
+  color: ${(props) => props.theme.primaryText};
+  height: 100vh;
+  width: 100vw;
+`;
