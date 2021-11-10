8217d916eb28dd33f5bc1d18e4558c9e05a72a78
Tammas Hicks <50436881+ScriptkidHicks@users.noreply.github.com>
2021-11-10 12:59:24 -0800

    Working with modifying token login

--- FrontEnd/frontend/src/Pages/MonsterPage.js
+++ FrontEnd/frontend/src/Pages/MonsterPage.js
@@ -1,8 +1,22 @@
 import { useContext } from "react";
+import { useNavigate } from "react-router";
 import styled from "styled-components";
 import { ColorContext } from "../ContextProviders/ColorContext";
 
+function parseJWT(token) {
+  try {
+    return JSON.parse(atob(token.split(",")[1]));
+  } catch (e) {
+    return null;
+  }
+}
+
 function MonsterPage() {
+  const navigate = useNavigate();
+  let token = localStorage.getItem("id_token");
+  console.log("token", token);
+  token = parseJWT(token);
+  console.log("parsed:", token);
   const theme = useContext(ColorContext);
   return <Body theme={theme}>Monster Page</Body>;
 }
