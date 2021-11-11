86a59a4447bada19eb80e82c81b0738523325699
Niklaas Cotta <nikcotta@gmail.com>
2021-11-11 10:57:00 -0800

    small changes to Fiend_skeleton

--- BackEnd/Fiend_Skeleton.py
+++ BackEnd/Fiend_Skeleton.py
@@ -5,7 +5,7 @@ Team WAHP, CIS422 FA21
 Desc:
 """
 
-import time
+from time import *
 
 """
 Hierarchy
@@ -27,75 +27,96 @@ Hierarchy
     intensity (int):  how intense the workout was
 """
 
+"""
+TODO:
+1) Image attribute implementation
+2) Global dictionary bestiary
+3) Evolution() method
+    - Need to figure out species stuff first
+4) Decorator??
+"""
+
 
 class Level:
     def __init__(self):
         self.value = 1
-        self.cap = 3
+        self.xpCap = 3
         self.xp = 0
 
     def __str__(self):
         return str(self.value)
 
-    def addxp(self, value):
-
+    def incrementXP(self, value):
         done = False
+        oldLevel = self.value
 
         print(f"EXP increased by {value} points!")
         self.xp += value
 
         while not done:
-            prevlevel = self.value
+            prevLevel = self.value
 
-            if self.xp >= self.cap:
-                self.levelup()
+            if self.xp >= self.xpCap:  # levelup
+                self.levelUp()
                 print(f"LEVELED UP TO LEVEL {str(self.value)}!!!")
 
-            if self.value == prevlevel:
+            if self.value == prevLevel:
                 done = True
 
-            time.sleep(0.25)
+            sleep(0.25)
 
-    def levelup(self):
+        return self.value - oldLevel  # how much you leveled up
+
+    def levelUp(self):
         self.value += 1
-        self.xp -= self.cap
+        self.xp -= self.xpCap
         self.resetCap()
 
+    # def levelDown(self):
+
     def resetCap(self):
-        self.cap = ((4 * self.value ** 3) // 5)
+        self.xpCap = ((4 * self.value ** 3) // 5)
 
 
 class Fiend:
-    def __init__(self, name, type_id, level):
+    def __init__(self, name, species, level):
         self.name = name
-        self.type_id = type_id
+        self.species = species
         self.level = level
 
     def dance(self):
         print(f"{self.name} does a little dance and looks very cute doing it")
 
-    def translateID(self):
-        translationTable = {
-            0o001: "Leg Monster",
-            0o002: "Arm Monster"
-        }
+    def updateXP(self, value):
+        leveled = self.level.incrementXP(value)
+        if leveled > 0:
+            # check if ready to transform
+            # if so, evolve
+            pass
 
-        return translationTable[self.type_id]
+    def transform(self):
+        prevName = self.name
+        print(f"{self.name} is evolving!!!")
+        for _ in range(3):
+            sleep(0.25)
+            print("  .  ")
+        # find next species (KM1 -> KM2 -> ...)
+        # set new species
+        # fetch image of species and set new image
+        print(f"{prevName} evolved into ... {self.name}!!!")
 
     def printinfo(self):
         print("==========================================")
         print(f"Name:     {self.name}")
-        print(f"Type:     {self.translateID()}")
-        print(f"Level:    {str(self.level.value)} (xp: {str(self.level.xp)}/{str(self.level.cap)})")
+        print(f"Species:  {self.species}")
+        print(f"Level:    {str(self.level.value)} (xp: {str(self.level.xp)}/{str(self.level.xpCap)})")
         print("==========================================\n")
 
 
 if __name__ == '__main__':
     newLevel = Level()
-    myFiend = Fiend("Lil Klokov", 0o001, newLevel)
-    myFiend.dance()
+    myFiend = Fiend("Kloks", "KM01", newLevel)
     for i in range(5):
-        time.sleep(1)
+        sleep(0.5)
         myFiend.printinfo()
-        myFiend.level.addxp(50)
-
+        myFiend.updateXP(50)
