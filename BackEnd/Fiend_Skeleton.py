4da25fac4c21d5e5593941ff0d94d4d89d8e5b93
Niklaas Cotta <nikcotta@gmail.com>
2021-11-12 16:10:17 -0800

    added evolution method and global bestiary, fixed species

--- BackEnd/Fiend_Skeleton.py
+++ BackEnd/Fiend_Skeleton.py
@@ -6,6 +6,7 @@ Desc:
 """
 
 from time import *
+import sys
 
 """
 Hierarchy
@@ -29,12 +30,37 @@ Hierarchy
 
 """
 TODO:
-1) Image attribute implementation
-2) Global dictionary bestiary
-3) Evolution() method
-    - Need to figure out species stuff first
-4) Decorator??
+1) Decorator??
+2) EXP rewarded w/ workout log entry is functional
 """
+
+bestiary = {"01": "Baby",
+            "02": "Juvenile",
+            "03": "Normal",
+            "04": "Mature",
+            "05": "Magnificent",
+            "06": "Geriatric",
+            "07": "Ascended"}
+
+
+def delay_print(s, delay):
+    # print one character at a time
+    # https://stackoverflow.com/questions/9246076/how-to-print-one-character-at-a-time-on-one-line
+    for c in s:
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        sleep(delay)
+
+
+def testFiend():
+    myFiend = Fiend()
+    myFiend.printinfo()
+    for i in range(5):
+        sleep(0.5)
+        print(f"\nStep: {i+1}")
+        myFiend.updateXP(50)
+    myFiend.printinfo()
+    myFiend.seppuku()
 
 
 class Level:
@@ -72,51 +98,56 @@ class Level:
         self.xp -= self.xpCap
         self.resetCap()
 
-    # def levelDown(self):
-
     def resetCap(self):
         self.xpCap = ((4 * self.value ** 3) // 5)
 
 
 class Fiend:
-    def __init__(self, name, species, level):
-        self.name = name
+    def __init__(self, nickname="Thammash Smotta", species="Kettlehell", species_id="KM01", level=Level()):  # :)
+        self.nickname = nickname
         self.species = species
+        self.id = species_id
         self.level = level
 
     def dance(self):
-        print(f"{self.name} does a little dance and looks very cute doing it")
+        print(f"{self.nickname} does a little dance and looks very cute doing it")
+
+    def tellLevel(self):
+        return self.level.value
 
     def updateXP(self, value):
         leveled = self.level.incrementXP(value)
-        if leveled > 0:
-            # check if ready to transform
-            # if so, evolve
-            pass
+        evolveLevel = 5
+        toEvolve = self.level.value % evolveLevel
+        if toEvolve == 0 and leveled > 0:
+            self.transform()
 
     def transform(self):
-        prevName = self.name
-        print(f"{self.name} is evolving!!!")
+        print(f"{self.nickname} is evolving!!!")
         for _ in range(3):
-            sleep(0.25)
+            sleep(0.5)
             print("  .  ")
-        # find next species (KM1 -> KM2 -> ...)
-        # set new species
-        # fetch image of species and set new image
-        print(f"{prevName} evolved into ... {self.name}!!!")
+
+        newFormInt = int(self.id[3]) + 1
+        self.id = self.id[0:3] + str(newFormInt) + self.id[4:]  # dont ask me
+        print(f"{self.species} grew into it's {bestiary[self.id[2:]]} form!!!")
+
+        return self.species
+
+    def seppuku(self):
+        delay_print("私は名誉をもって人生を送ってきました。 私は何も後悔していない...\n", 0.15)
+        sleep(1)
+        delay_print("死  DEATH  死\n", 0.25)
+        sleep(1)
+        del self
 
     def printinfo(self):
         print("==========================================")
-        print(f"Name:     {self.name}")
-        print(f"Species:  {self.species}")
+        print(f"Name:     {self.nickname}")
+        print(f"Species:  {self.species} ({bestiary[self.id[2:]]})")
         print(f"Level:    {str(self.level.value)} (xp: {str(self.level.xp)}/{str(self.level.xpCap)})")
         print("==========================================\n")
 
 
 if __name__ == '__main__':
-    newLevel = Level()
-    myFiend = Fiend("Kloks", "KM01", newLevel)
-    for i in range(5):
-        sleep(0.5)
-        myFiend.printinfo()
-        myFiend.updateXP(50)
+    testFiend()
