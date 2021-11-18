8a62317bdf8653a50116aa124366da8de7f57932
Tammas Hicks <50436881+ScriptkidHicks@users.noreply.github.com>
2021-11-18 15:21:32 -0800

    Completed inline documentation

--- FrontEnd/frontend/src/Pages/AccountPage.js
+++ FrontEnd/frontend/src/Pages/AccountPage.js
@@ -4,7 +4,9 @@ import { ColorContext } from "../ContextProviders/ColorContext";
 import RibbonBar from "../Components/RibbonBar";
 
 function AccountPage() {
+  // used to provide the "theme" color prop to all elements the reference the prop in their css
   const theme = useContext(ColorContext);
+  // page targets and page titles are distributed individually to each page. We don't want a page to have a load button for the page that it is already on. Make sure that the page targets correlate correctly to the page titles. They will be unpacked in the same order.
   const pageTargets = ["/MonsterPage", "/WorkoutLogPage", "/PastWorkoutsPage"];
   const pageTitles = [
     "Your Monster",
