7b707c2f90d8921d770d982c49136ec8ae4dc7c3
Jordan Smith <jssmith9876@yahoo.com>
2021-12-01 19:43:00 -0800

    Fixed submit button issue, submitting a workout now rewards monster experience

--- FrontEnd/frontend/src/Pages/WorkoutLogPage.js
+++ FrontEnd/frontend/src/Pages/WorkoutLogPage.js
@@ -12,6 +12,7 @@ function WorkoutLogPage() {
   if (userToken) {
     userToken = jwtDecode(userToken);
     userId = userToken.user_id;
+    console.log(userId);
     if (userToken.exp * 1000 < Date.now()) {
       navigate("/SignIn");
     }
@@ -35,7 +36,7 @@ function WorkoutLogPage() {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        user_token: 1,
+        user_token: userId,
       },
       body: JSON.stringify({
         hasCompleted: workoutCompleted,
