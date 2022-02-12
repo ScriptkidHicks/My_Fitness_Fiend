7b2da3a81cdaa54d4661abcd4c0d9fe8f706f754
tjoyce <tjoyce@uoregon.edu>
2022-02-11 17:52:24 -0800

    small changes to workout_classes

--- BackEnd/Workout_Classes.py
+++ BackEnd/Workout_Classes.py
@@ -2,7 +2,7 @@
 code: Creating the Workout classes and subclasses
 group: Workout assistance Program
 author(s): Thomas Joyce
-last modified: 17 Nov 2021
+last modified: 11 Feb 2022
 '''
 from db_manager import *
 from collections import defaultdict
